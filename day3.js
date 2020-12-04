ea94dc41c22bc32ca66ff732a16e5cc4806dd8b4
dianatofan <diana_tofan@ymail.com>
2020-12-04 22:28:00 +0100

    Add day3 part 2

--- day3.js
+++ day3.js
@@ -3,20 +3,7 @@
 
 import { getInput } from "./getInput.js";
 
-const test = `..##.........##.........##.........##.........##.........##.......
-#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
-.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-.#........#.#........#.#........#.#........#.#........#.#........#
-#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
-#...##....##...##....##...##....##...##....##...##....##...##....#
-.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"`;
-
-// Part 1: find out how many you'd encounter following a slope of right 3 and down 1
-(async () => {
+const getNumberOfTreesEncountered = async (right, down) => {
   let inputData = await getInput(3);
   inputData = inputData.map(item => item.split(""));
   const width = inputData[0].length;
@@ -28,8 +15,49 @@ const test = `..##.........##.........##.........##.........##.........##.......
   do {
     pos = inputData[j][i % width];
     pos === "#" && count++;
-    i += 3;
-    j += 1;
+    i += right;
+    j += down;
   } while (j < height);
+  return count;
+};
+
+// Part 1: find out how many you'd encounter following a slope of right 3 and down 1
+(async () => {
+  const count = await getNumberOfTreesEncountered(3, 1);
   console.log(count);
+})();
+
+// Part 2: multiply numberr of trees on multiple slopes
+(async () => {
+  const slopes = [
+    {
+      right: 1,
+      down: 1
+    },
+    {
+      right: 3,
+      down: 1
+    },
+    {
+      right: 5,
+      down: 1
+    },
+    {
+      right: 7,
+      down: 1
+    },
+    {
+      right: 1,
+      down: 2
+    }
+  ];
+  let product = 1;
+  const getTrees = async () =>
+    Promise.all(
+      slopes.map(slope => getNumberOfTreesEncountered(slope.right, slope.down))
+    );
+  getTrees().then(data => {
+    data.forEach(item => (product *= item));
+    console.log(product);
+  });
 })();
