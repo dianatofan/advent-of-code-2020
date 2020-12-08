a3025c5a47445a7168cabffc4a56a9088b2c88ef
dianatofan <diana_tofan@ymail.com>
2020-12-08 19:33:26 +0100

    Add day7 part 2

--- day7.js
+++ day7.js
@@ -18,34 +18,47 @@ import { getInput } from "./getInput.js";
           const { groups } = /((?<number>\d+) )?(?<color>.*)/.exec(
             txt.replace(/ bags?/, "")
           );
-          // console.log(groups);
           if (!map.has(bagColor)) {
             map.set(bagColor, []);
           }
           if (!groups.number) groups.number = 0;
           map.set(bagColor, [...map.get(bagColor), groups]);
         });
-
-    const containsShinyGold = color => {
-      if (color === "shiny gold") return true;
-      if (!map.has(color)) return false;
-
-      const bagsWithin = map.get(color);
-      for (const { color: bag } of bagsWithin) {
-        if (containsShinyGold(bag)) {
-          return true;
-        }
-      }
-      return false;
-    };
-
-    const colors = map.keys();
-    let count = 0;
-
-    for (const color of colors) {
-      containsShinyGold(color) && color != "shiny gold" && count++;
-    }
-
-    console.log(count);
   });
+
+  const containsShinyGold = color => {
+    if (color === "shiny gold") return true;
+    if (!map.has(color)) return false;
+
+    const bagsWithin = map.get(color);
+    for (const { color: bag } of bagsWithin) {
+      if (containsShinyGold(bag)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const colors = map.keys();
+  let count = 0;
+
+  for (const color of colors) {
+    containsShinyGold(color) && color != "shiny gold" && count++;
+  }
+
+  console.log(count);
+
+  // Part 2: How many individual bags are required inside your single shiny gold bag?
+  const getNrOfBags = containerBag => {
+    if (containerBag.number == 0) return 0;
+
+    const bagsWithin = map.get(containerBag.color);
+    let sum = 1;
+    for (const bag of bagsWithin) {
+      sum += bag.number * getNrOfBags(bag);
+    }
+    return sum;
+  };
+
+  console.log(getNrOfBags({ number: 1, color: "shiny gold" }) - 1);
 })();
