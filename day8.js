c76c1f26dae1d383246c41c6decab4d9aa6f2504
dianatofan <diana_tofan@ymail.com>
2020-12-09 01:18:06 +0100

    Add day8 part 2

--- day8.js
+++ day8.js
@@ -3,39 +3,63 @@
 
 import { getInput } from "./getInput.js";
 
-// Part 1: find the acc value before instructions repeat
 (async () => {
   const instructions = await getInput(8);
-  const test = `nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6`;
-  let acc = 0,
-    index = 0;
+  const executeProgram = instructions => {
+    let acc = 0,
+      index = 0;
+    const visitedIndexes = new Set();
+    do {
+      // console.log(index, instructions.length);
+      if (index === instructions.length - 1) {
+        console.log("end: ", acc);
+        return false;
+      }
+      if (visitedIndexes.has(index)) {
+        console.log("infinite loop: ", acc);
+        return true;
+      }
+      visitedIndexes.add(index);
+      const [opCode, arg] = instructions[index].split(" ");
+      switch (opCode) {
+        case "acc":
+          acc += parseInt(arg);
+          index++;
+          break;
+        case "jmp":
+          index += parseInt(arg);
+          break;
+        case "nop":
+          index++;
+          break;
+      }
+    } while (true);
+  };
 
-  const visitedIndexes = [];
+  // Part 1: find the acc value before instructions repeat
+  console.log(executeProgram(instructions));
 
-  do {
-    visitedIndexes.push(index);
-    const [opCode, arg] = instructions[index].split(" ");
-    switch (opCode) {
-      case "acc":
-        acc += parseInt(arg);
-        index++;
-        break;
-      case "jmp":
-        index += parseInt(arg);
-        break;
-      case "nop":
-        index++;
-        break;
-    }
-  } while (!visitedIndexes.includes(index));
+  // Part 2: fix infinite loop by swapping a 'jmp' with 'nop' or viceversa
+  const findCorruptedInstruction = program => {
+    let hasInfiniteLoop = true,
+      replacedOpIndex = 0;
+    const jmpAndNops = program.filter(
+      instruction =>
+        instruction.indexOf("nop") > -1 || instruction.indexOf("jmp") > -1
+    );
+    do {
+      const [opCode, arg] = jmpAndNops[replacedOpIndex].split(" ");
+      const newProgram = program.map(instruction =>
+        instruction === jmpAndNops[replacedOpIndex]
+          ? `${
+              opCode === "nop" ? "jmp" : opCode === "jmp" ? "nop" : "acc"
+            } ${arg}`
+          : instruction
+      );
+      hasInfiniteLoop = executeProgram(newProgram);
+      replacedOpIndex++;
+    } while (hasInfiniteLoop && jmpAndNops[replacedOpIndex]);
+  };
 
-  console.log(acc);
+  findCorruptedInstruction(instructions);
 })();
