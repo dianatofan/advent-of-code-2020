cd035e5e2f94443b93c639c6258cca8e5c4f8210
dianatofan <diana_tofan@ymail.com>
2020-12-19 01:24:54 +0100

    Add day18 part 2

--- day18.js
+++ day18.js
@@ -6,17 +6,29 @@ import { getInput } from "./getInput.js";
 (async () => {
   const expressions = await getInput(18);
 
-  const solveExpression = expression => {
+  const solveExpression = (expression, hasAdditionPrecedence) => {
+    if (hasAdditionPrecedence) {
+      if (expression.indexOf("+") > -1) {
+        do {
+          expression = expression.replace(
+            /(\d+) \+ (\d+)/g,
+            (match, number1, number2) => parseInt(number1) + parseInt(number2)
+          );
+        } while (expression.indexOf("+") > -1);
+      }
+    }
     const numbers = expression.match(/\d+/g);
     const symbols = expression.match(/[+*]/g);
     let result = 0;
-    symbols.forEach((symbol, i) => {
-      result = eval(`${result || numbers[i]} ${symbol} ${numbers[i + 1]}`); // drop operators precedence
-    });
-    return result;
+    symbols &&
+      symbols.forEach((symbol, i) => {
+        result = eval(`${result || numbers[i]} ${symbol} ${numbers[i + 1]}`); // drop operators precedence
+      });
+    return hasAdditionPrecedence ? eval(expression) : result;
   };
 
-  let sum = 0;
+  // Part 1: solve expressions with operators precedence changed left-to-right
+  let sum1 = 0;
   expressions.forEach(expression => {
     if (expression.indexOf("(") > -1) {
       do {
@@ -25,8 +37,27 @@ import { getInput } from "./getInput.js";
         );
       } while (expression.indexOf("(") > -1);
     }
-    sum += solveExpression(expression);
+    sum1 += solveExpression(expression);
   });
 
-  console.log(sum);
+  console.log("Part 1: ", sum1);
+
+  const test = [`5 + (8 * 3 + 9 + 3 * 4 * 3)`];
+
+  // Part 2: solve expressions with operators precedence changed - addition before multiplication
+  let sum2 = 0;
+
+  expressions.forEach(expression => {
+    if (expression.indexOf("(") > -1) {
+      do {
+        expression = expression.replace(/\(([^()]+)\)/g, (match, group) =>
+          solveExpression(group, true)
+        );
+      } while (expression.indexOf("(") > -1);
+    }
+    expression = solveExpression(expression, true);
+    sum2 += expression;
+  });
+
+  console.log("Part 2: ", sum2);
 })();
